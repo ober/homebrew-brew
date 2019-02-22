f1db2991a5412d09782c81f3c1228a629236ea99
ober <jaimef@linbsd.org>
2019-02-21 19:37:05 -0800

    update to 15.1

--- gerbil-scheme-ssl.rb
+++ gerbil-scheme-ssl.rb
@@ -1,8 +1,8 @@
 class GerbilSchemeSsl < Formula
   desc "Opinionated dialect of Scheme designed for Systems Programming"
   homepage "https://cons.io"
-  url "https://github.com/vyzo/gerbil/archive/v0.14.tar.gz"
-  sha256 "8525877960ba7a6122d6dfd2bae34ed1437d5231f9e31de97bbe6106c4dfa03a"
+  url "https://github.com/vyzo/gerbil/archive/v0.15.1.tar.gz"
+  sha256 "3d29eecdaa845b073bf8413cd54e420b3f48c79c25e43fab5a379dde029d0cde"
 
   bottle do
     root_url "https://github.com/ober/homebrew-brew/raw/master"
