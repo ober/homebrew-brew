89eb91e34565e47a48bdee3b4e412ea29fa6a423
ober <jaimef@linbsd.org>
2019-05-14 12:23:11 -0700

    add fix for latest gambit

--- gambit-scheme-current.rb
+++ gambit-scheme-current.rb
@@ -24,7 +24,7 @@ class GambitSchemeCurrent < Formula
       s.gsub! 'SSL_VERIFY_PEER', 'SSL_VERIFY_NONE'
     end
 
-    ENV['CC'] =  Formula['gcc'].opt_bin/Formula['gcc'].aliases.first.gsub("@","-")
+    ENV['CC'] =  "#{Formula['gcc'].opt_bin/Formula['gcc'].aliases.first.gsub("@","-")} -D___USE_C_RTS_CHAR_OPERATIONS"
     system "./configure", *args
     system "make"
     ENV.deparallelize
