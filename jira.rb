95ab01891f68789080c6b33ed8e238639816ea66
ober <jaimef@linbsd.org>
2019-10-03 12:50:15 -0700

    bump version

--- jira.rb
+++ jira.rb
@@ -2,7 +2,7 @@ class Jira < Formula
   desc "jira command line helper"
   homepage "https://github.com/ober/jira"
   url "https://github.com/ober/jira.git"
-  version "0.02"
+  version "0.04"
 
   depends_on "gerbil-scheme-ssl" => :build
 
