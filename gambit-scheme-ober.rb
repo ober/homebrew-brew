a55ad5e0c3649dc481f903f8a394cc8faab8284c
ober <jaimef@linbsd.org>
2019-10-12 21:18:48 -0400

    fix up

--- gambit-scheme-ober.rb
+++ gambit-scheme-ober.rb
@@ -4,8 +4,8 @@ class GambitSchemeOber < Formula
   url "https://github.com/gambit/gambit/archive/v4.9.3.tar.gz"
   sha256 "a5e4e5c66a99b6039fa7ee3741ac80f3f6c4cff47dc9e0ff1692ae73e13751ca"
 
-  depends_on "openssl"
-  depends_on "gcc"
+  depends_on "openssl@1.1"
+  depends_on "gcc@9"
 
   bottle do
     rebuild 1
