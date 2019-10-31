d9fefcd662e99cb08c3a9ad4e9b075442367520f
ober <jaimef@linbsd.org>
2019-10-30 21:39:21 -0700

    fix typo

--- jira.rb
+++ jira.rb
@@ -12,7 +12,7 @@ class Jira < Formula
     gambit = Formula["gambit-scheme-ober"]
     ENV['GERBIL_HOME'] = gambit.libexec
     ENV['CC'] =  Formula['gcc'].opt_bin/Formula['gcc'].aliases.first.gsub("@","-")
-    ENV.apppend_path "PATH", "#{gambit.opt_prefix}/current/bin"
+    ENV.append_path "PATH", "#{gambit.opt_prefix}/current/bin"
     #bin.install Dir["./jira"]
     ENV['GERBIL_PATH'] = "."
     ENV['GERBIL_HOME'] = "/usr/local/opt/gerbil-scheme-ober/libexec"
