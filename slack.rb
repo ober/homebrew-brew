17488a26e96004e45aaf0fc81e6f0c72268bf8f1
ober <jaimef@linbsd.org>
2019-11-06 22:26:03 -0800

    bump slack.rb to 0.03

--- slack.rb
+++ slack.rb
@@ -2,7 +2,7 @@ class Slack < Formula
   desc "Slack command line helper"
   homepage "https://github.com/ober/slack"
   url "https://github.com/ober/slack.git"
-  version "0.02"
+  version "0.03"
 
   bottle do
     root_url "https://github.com/ober/homebrew-brew/raw/master"
