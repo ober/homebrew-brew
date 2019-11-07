0f92f19a7c2eeeed78a1133f0209a043c4529fe4
ober <jaimef@linbsd.org>
2019-11-06 18:03:12 -0800

    back to normal

--- jira.rb
+++ jira.rb
@@ -1,7 +1,7 @@
 class Jira < Formula
   desc "jira command line helper"
   homepage "https://github.com/ober/jira"
-  head "https://github.com/ober/jira.git"
+  url "https://github.com/ober/jira.git"
 
   version "0.07"
   depends_on "gerbil-scheme-ober"
