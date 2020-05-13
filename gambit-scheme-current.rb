39f88affb4c4520730197dae1109dbf7d9891ebd
ober <jaimef@linbsd.org>
2020-05-12 20:30:06 -0700

    add steps for head

--- gambit-scheme-current.rb
+++ gambit-scheme-current.rb
@@ -21,7 +21,10 @@ class GambitSchemeCurrent < Formula
     #ENV['CC'] = "#{Formula['gcc'].opt_bin/Formula['gcc'].aliases.first.gsub("@","-")}"
 
     system "./configure", *args
+    system "make", "bootstrap"
+    system "make", "bootclean"
     system "make"
+    system "make", "modules"
     ENV.deparallelize
     system "make", "install"
   end
