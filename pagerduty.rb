3ff607ea8012476139c31649c08868507aae2750
jfournier <jfournier@castlighthealth.com>
2020-10-05 14:26:30 -0700

    bump

--- pagerduty.rb
+++ pagerduty.rb
@@ -8,7 +8,7 @@ class Pagerduty < Formula
 
   bottle do
     root_url "https://github.com/ober/homebrew-artifacts/raw/master"
-    sha256 "57eaed33c38aab51d0f6462868ab0f01d5ecacdf6fad2e1c8c05d3b94524ad54" => :mojave
+    sha256 "0b60d3c3eb0638a8b1f6a04333f0100f3ccc3cc199e2e8968c52aa5a12468115" => :mojave
   end
 
   def install
