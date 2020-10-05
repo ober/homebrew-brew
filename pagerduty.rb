bee122c8cd0145ecae394e709a5500489944b0f6
jfournier <jfournier@castlighthealth.com>
2020-10-05 14:29:34 -0700

    fix for catalina

--- pagerduty.rb
+++ pagerduty.rb
@@ -8,7 +8,7 @@ class Pagerduty < Formula
 
   bottle do
     root_url "https://github.com/ober/homebrew-artifacts/raw/master"
-    sha256 "0b60d3c3eb0638a8b1f6a04333f0100f3ccc3cc199e2e8968c52aa5a12468115" => :mojave
+    sha256 "0b60d3c3eb0638a8b1f6a04333f0100f3ccc3cc199e2e8968c52aa5a12468115" => :catalina
   end
 
   def install
