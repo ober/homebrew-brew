cbb429b1f8f90156c7bdb0129950e207175d02df
jfournier <jfournier@castlighthealth.com>
2020-10-19 10:15:45 -0700

    bump confluence

--- confluence.rb
+++ confluence.rb
@@ -4,7 +4,7 @@ class Confluence < Formula
   url "https://github.com/ober/confluence.git"
 
   depends_on "gerbil-scheme-ober" => :build
-  version "0.11"
+  version "0.12"
 
   bottle do
     root_url "https://github.com/ober/homebrew-artifacts/raw/master"
