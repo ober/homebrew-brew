3881008b180e0f69f195be6c397754fffc8e20cf
jfournier <jfournier@castlighthealth.com>
2020-12-17 13:40:28 -0800

    bump jira

--- jira.rb
+++ jira.rb
@@ -3,7 +3,7 @@ class Jira < Formula
   homepage "https://github.com/ober/jira"
   url "https://github.com/ober/jira.git"
 
-  version "0.16"
+  version "0.17"
   depends_on "gerbil-scheme-ober"
 
   bottle do
