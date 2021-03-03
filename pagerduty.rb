e9678cd9d4473bdaa22f7a75dd0a98b792c1d3ce
jfournier <jfournier@castlighthealth.com>
2021-03-03 14:24:14 -0800

    bump

--- pagerduty.rb
+++ pagerduty.rb
@@ -2,7 +2,7 @@ class Pagerduty < Formula
   desc "Pagerduty command line helper"
   homepage "https://github.com/ober/datadog"
   url "https://github.com/ober/datadog.git"
-  version "0.11"
+  version "0.12"
 
   depends_on "gerbil-scheme-ober" => :build
 
