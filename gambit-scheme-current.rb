f0281bc97764ffbd5346b665f93dd65300fa87da
jfournier <jfournier@castlighthealth.com>
2021-06-01 19:26:54 -0700

    add items from jazz

--- gambit-scheme-current.rb
+++ gambit-scheme-current.rb
@@ -7,24 +7,27 @@ class GambitSchemeCurrent < Formula
   head "https://github.com/gambit/gambit.git", revision: "6aed5fa0" # good "b3455576"
 
   depends_on "openssl@1.1"
-  # depends_on "gcc" => :build
+  depends_on "gcc" => :build
 
   def install
     args = %W[
     --enable-default-runtime-options=f8,-8,t8
-    --enable-openssl
     --enable-multiple-versions
+    --enable-openssl
+    --enable-rtlib-debug-environments
+    --enable-rtlib-debug-location
+    --enable-single-host
     --enable-smp
+    --enable-track-scheme
     --prefix=#{prefix}
     ]
 
-# --enable-single-host
+    #inreplace "lib/os_io.c" do |s|
+    #  s.gsub! 'SSL_VERIFY_PEER', 'SSL_VERIFY_NONE'
+    #end
 
-    # inreplace "lib/os_io.c" do |s|
-    #   s.gsub! 'SSL_VERIFY_PEER', 'SSL_VERIFY_NONE'
-    # end
-    # ENV['CC'] = "#{Formula['gcc'].opt_bin/Formula['gcc'].aliases.first.gsub("@","-")}"
-
+    ENV['CC'] = "#{Formula['gcc'].opt_bin/Formula['gcc'].aliases.first.gsub("@","-")}"
+    ENV['CFLAGS'] = '-D___DONT_USE_BUILTIN_SETJMP'
     system "./configure", *args
     system "make", "bootstrap"
     system "make", "bootclean"
